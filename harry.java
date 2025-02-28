3b667ffb4b3578c4bab45ae82d52c0f0cd6c3165
jashan dhillon <jashandhillon595@gmail.com>
2025-02-28 10:35:24 +0530

    [jashan] ADD:harry.java

---
+++ harry.java
@@ -0,0 +1,12 @@
+public class harry {
+    public static void main(String[] args) {
+        int birthYear = 2000;
+        int currentYear = 2024;
+        
+        // Calculate age
+        int age = currentYear - birthYear;
+        
+        // Output the result
+        System.out.println("Harry's age in " + currentYear + " is " + age);
+    }
+}
